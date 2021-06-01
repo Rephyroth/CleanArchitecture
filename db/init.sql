f1c37b1f708e9415c240f5917a1a540fda76cc11
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2021-06-01 18:06:51 -0500

    commit tables

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,33 @@
-CREATE TABLE books (
-	id SERIAL NOT NULL,
-	nombre VARCHAR(50),
-	autor VARCHAR(50),
-	PRIMARY KEY (id)
+CREATE TABLE author(
+	id_author SERIAL NOT NULL,
+	name VARCHAR(50),
+	tel VARCHAR(15),
+	PRIMARY KEY (id_author)
+);
+
+CREATE TABLE publisher(
+	id_publisher SERIAL NOT NULL,
+	name VARCHAR(50),
+	tel VARCHAR(15),
+	address VARCHAR(100),
+	PRIMARY KEY (id_publisher)
+);
+
+
+CREATE TABLE book(
+	id_book SERIAL NOT NULL,
+	title VARCHAR(100),
+	isbn VARCHAR(50),
+	author INTEGER(11),
+	editorial INTEGER(11),
+	genre VARCHAR(50),
+	num_pages VARCHAR(10),
+	year VARCHAR(10),
+	PRIMARY KEY (id_book),
+	CONSTRAINT fk_author
+		FOREIGN KEY(author)
+			REFERENCES author(id_author),
+	CONSTRAINT fk_publisher
+		FOREIGN KEY(publisher)
+			REFERENCES publisher(id_publisher)
 );
