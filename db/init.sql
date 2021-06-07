576144e44382a0b0a2b2fe24edb2f6c39179ca57
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2021-06-07 16:10:27 -0500

    commit sql changes

--- db/init.sql
+++ db/init.sql
@@ -1,33 +1,11 @@
-CREATE TABLE author(
-	id_author INT GENERATED ALWAYS AS IDENTITY,
-	name VARCHAR(50),
-	tel VARCHAR(15),
-	PRIMARY KEY (id_author)
-);
-
-CREATE TABLE publisher(
-	id_publisher INT GENERATED ALWAYS AS IDENTITY,
-	name VARCHAR(50),
-	tel VARCHAR(15),
-	address VARCHAR(100),
-	PRIMARY KEY (id_publisher)
-);
-
-
 CREATE TABLE book(
 	id_book INT GENERATED ALWAYS AS IDENTITY,
 	title VARCHAR(100),
 	isbn VARCHAR(50),
-	author INT,
-	publisher INT,
+	author VARCHAR(50),
+	publisher VARCHAR(50),
 	genre VARCHAR(50),
 	num_pages VARCHAR(10),
 	year VARCHAR(10),
 	PRIMARY KEY (id_book),
-	CONSTRAINT fk_author
-		FOREIGN KEY(author)
-			REFERENCES author(id_author),
-	CONSTRAINT fk_publisher
-		FOREIGN KEY(publisher)
-			REFERENCES publisher(id_publisher)
 );
