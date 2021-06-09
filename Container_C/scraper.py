c5ceb63d7ae7154bf0d16ba82aec701618a9bbc6
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2021-06-08 20:28:58 -0500

    Finalizar Container C

--- Container_C/scraper.py
+++ Container_C/scraper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json 
 from models import Book
 
@@ -6,10 +5,10 @@ with open('MOCK_DATA.json') as json_file:
     data = json.load(json_file)
 
 for row in data:
-    book = Book(id_book = row.get("id_book"),
+    book = Book(
                 title = row.get("title"),
-                isbn = row.get("isbn"),
-                author = row.get("author"),
+                isbn = row.get("isbn "),
+                author = row.get("author "),
                 publisher = row.get("publisher"),
                 genre = row.get("genre"),
                 num_pages = row.get("num_pages"),
@@ -20,24 +19,3 @@ for row in data:
 
         
     
-=======
-
-import json 
-from models import Book
-
-
-with open('MOCK_DATA.json') as json_file:
-    data = json.load(json_file)
-
-
-for row in data:
-    book = Book(id_book = row.get("id_book"),
-                title = row.get("title"),
-                isbn = row.get("isbn"),
-                author = row.get("author"),
-                publisher = row.get("publisher"),
-                genre = row.get("genre"),
-                num_pages = row.get("num_pages"),
-                year = row.get("year"))
-        
->>>>>>> ed9c3e59d320a6e84707768f8cb315a73a133f18
